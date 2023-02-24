8eba60c4a978dd2f26909725ff7a9db8b936f003
nishamore98 <69397500+nishamore98@users.noreply.github.com>
2023-02-25 01:53:41 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './style.css';
+import Home from './Home';
 
 export default function App() {
   return (
     <div>
-      <h1>www.nishamore.tech</h1>
-      <p>Coming soon :)</p>
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, I'm Nisha</h1>
+      <p>React developer</p>
+      <img src="./pic.jpeg" width="50" />
+    </div>
+  );
+}
