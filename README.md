dc485f2e1b74e7a9d0e75ab1b384ff87c4b5e2f2
nishamore98 <69397500+nishamore98@users.noreply.github.com>
2023-02-25 00:03:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xzyybf)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>www.nishamore.tech</h1>
+      <p>Coming soon :)</p>
+    </div>
+  );
+}
