1991bb6c2c2794dba0434797be58cd3a2d260b38
nishamore98 <69397500+nishamore98@users.noreply.github.com>
2023-02-25 02:44:04 +0530

    3 files modified

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,10 @@ import './style.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello, I'm Nisha</h1>
-      <p>React developer</p>
-      <img src="./pic.jpeg" width="50" />
+    <div className="home-wrapper">
+      <h1 className="mb-0">Hello, I'm Nisha</h1>
+      <h2 className="mt-0">React developer</h2>
+      <img className="Logo" width="1000" height="500" />
     </div>
   );
 }
