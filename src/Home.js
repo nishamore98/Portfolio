34631b36330a3324755431bbe3a9e8125d21892e
nishamore98 <69397500+nishamore98@users.noreply.github.com>
2023-02-25 14:27:55 +0530

    3 files modified

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './style.css';
+import Button from '@mui/material/Button';
 
 export default function Home() {
+  const contactInfo = () => {
+    window.location.href = 'https://www.linkedin.com/in/nisha-more-a58b72146/';
+  };
   return (
-    <div className="home-wrapper">
-      <div className="title-container">
-        <h1 className="mb-0">Hello, I'm Nisha</h1>
-        <h2 className="mt-0">React developer</h2>
+    <>
+      <div className="home-wrapper">
+        <div className="title-container">
+          <h1 className="mb-0">Hello, I'm Nisha</h1>
+          <h2 className="mt-0">React developer</h2>
+          <Button variant="contained" color="secondary" onClick={contactInfo}>
+            Get in touch
+          </Button>
+        </div>
+        <img className="Logo" width="150" height="150" />
       </div>
-      <img className="Logo" width="150" height="150" />
-    </div>
+    </>
   );
 }
