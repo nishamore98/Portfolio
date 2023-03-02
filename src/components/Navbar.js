5de0579017240966785ce495989749e9cae6efb2
nishamore98 <69397500+nishamore98@users.noreply.github.com>
2023-03-02 16:29:38 +0530

    3 files modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className="bg-black">
-      <Link to="/">Home</Link>
-      {/* <a href="/">Projects</a> */}
-      <Link to="/Skills">Skills</Link>
+      <div>
+        <Link to="/">Home</Link>
+        {/* <a href="/">Projects</a> */}
+        <Link to="/Skills">Skills</Link>
+      </div>
+      <a href="../NishaResume.pdf" download className="cv-btn">
+        Download CV
+      </a>
     </nav>
   );
 }
